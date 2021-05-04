871f2a0b92b13f4109529430207cdc72526666d9
kaniluck <takahashikeisuke.0514@gmail.com>
2021-05-04 10:24:23 +0000

    [Finish] 5/4

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,14 @@
 class PostsController < ApplicationController
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to root_path
   end
 
   def index
@@ -13,6 +18,12 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+   private
+
+  def post_params
+    params.require(:post).permit(:title, :image, :text)
   end
 
 end
