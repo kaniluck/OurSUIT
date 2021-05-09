b0825e51e7f401dbab290a159f2d11ba10e739d6
kaniluck <takahashikeisuke.0514@gmail.com>
2021-05-09 10:01:02 +0000

    [Add] ランキング機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
   end
 
   def index
-    @posts = Post.all
+    @posts = Post.includes(:users).sort {|a,b| b.users.size <=> a.users.size}
   end
 
   def show
