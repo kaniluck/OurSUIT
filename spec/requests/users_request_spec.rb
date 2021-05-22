13ddccf8e66e69390829a5bf66fc172ba5840839
kaniluck <takahashikeisuke.0514@gmail.com>
2021-05-22 02:52:50 +0000

    [Add] RSpecテスト追加

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,77 +1,68 @@
 require 'rails_helper'
 
-RSpec.describe "postコントローラーのテスト", type: :request do
+RSpec.describe "userコントローラーのテスト", type: :request do
   let(:user) { create(:user) }
   let(:post) { create(:post) }
   describe 'ログイン済み' do
     before do
       sign_in user
     end
-    context "ランキングページが正しく表示される" do
+    context "詳細ページが正しく表示される" do
       before do
-        get posts_path
-      end
-      it 'リクエストは200 OKとなること' do
-        expect(response.status).to eq 200
-      end
-      it 'タイトルが正しく表示されていること' do
-        expect(response.body).to include("ランキング")
-      end
-    end
-
-    context "投稿詳細ページが正しく表示される" do
-      before do
-        get post_path(post)
+        get user_path(user)
       end
       it 'リクエストは200 OKとなること' do
         expect(response.status).to eq 200
       end
     end
 
-    context "いいね一覧ページが正しく表示される" do
+    context "プロフィール編集ページが正しく表示される" do
       before do
-        get likes_index_post_path(user)
+        get edit_user_path(user)
       end
       it 'リクエストは200 OKとなること' do
         expect(response.status).to eq 200
       end
       it 'タイトルが正しく表示されていること' do
-        expect(response.body).to include("いいね一覧")
+        expect(response.body).to include("プロフィールを編集")
+      end
+    end
+
+    context "タイムラインページが正しく表示される" do
+      before do
+        get time_line_user_path(user)
+      end
+      it 'リクエストは200 OKとなること' do
+        expect(response.status).to eq 200
+      end
+      it 'タイトルが正しく表示されていること' do
+        expect(response.body).to include("タイムライン")
       end
     end
 
   end
   describe '非ログイン' do
-    context "ランキングページへ遷移されない" do
+    context "詳細ページへ遷移されない" do
       before do
-        get posts_path
+        get user_path(user)
       end
       it 'リクエストは302 OKとなること' do
         expect(response.status).to eq 302
       end
     end
 
-    context "投稿詳細ページへ遷移されない" do
+    context "プロフィール編集ページへ遷移されない" do
       before do
-        get post_path(post)
+        get edit_user_path(user)
       end
       it 'リクエストは302 OKとなること' do
         expect(response.status).to eq 302
       end
     end
 
-    context "いいね一覧ページへ遷移されない" do
+    context "タイムラインページへ遷移されない" do
       before do
-        get likes_index_post_path(user)
-      end
-      it 'リクエストは302 OKとなること' do
-        expect(response.status).to eq 302
-      end
-    end
-
-    context "新規投稿ページへ遷移されない" do
-      before do
-        get new_post_path
+        get time_line_user_path(user)
       end
       it 'リクエストは302 OKとなること' do
         expect(response.status).to eq 302
